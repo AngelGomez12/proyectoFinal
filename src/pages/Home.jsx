348eee4ab2551d7ba2f1d3693ac83332b5cdb7ff
Rony Romero Duque @ronygrafo <ronygrafo@icloud.com>
2023-10-20 16:17:17 -0500

    Secnd try

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,12 +10,13 @@ export const Home = () => {
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Renta de Maquinaria Pesada</h1>
-          <h3 className="mb-5 text-5xl font-bold">La solución perfecta para tu proyecto</h3>
+          <h1 className="mb-5 text-4xl font-extrabold">Renta de Maquinaria Pesada</h1>
+          <h3 className="mb-5 text-2xl font-bold">La solución perfecta para tu proyecto</h3>
           <p className="mb-5">
           Obtén el equipo que necesitas, cuando lo necesitas y sin necesidad de comprarlo
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn btn-primary">Ver Máquinas Disponibles</button>
+          <button className="btn btn-link">Explora por categorías</button>
         </div>
       </div>
     </div>
