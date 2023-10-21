d58ceef6b82a3d5c397f06833bff24cd1ee7b30b
Rony Romero Duque @ronygrafo <ronygrafo@icloud.com>
2023-10-21 14:56:19 -0500

    Formatted

--- src/components/Categorie.jsx
+++ src/components/Categorie.jsx
@@ -1,19 +1,20 @@
 const Categorie = (props) => {
-    console.log(props);
+  console.log(props);
   return (
     <div className="card bg-base-100 shadow-xl image-full w-72 h-[200px]">
-              <img className="object-cover rounded-2xl "
-                src={props.image}
-                alt="Shoes"
-              />
-            <div className="card-body h-[200px]" >
-              <h2 className="card-title  text-primary-content">{props.title}</h2>
-              <p className=" text-primary-content">{props.subtitle}</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-accent btn-m">Ver Todos</button>
-              </div>
-            </div>
-          </div>
-  )
-}
-export default Categorie
+      <img
+        className="object-cover rounded-2xl "
+        src={props.image}
+        alt="Shoes"
+      />
+      <div className="card-body h-[200px]">
+        <h2 className="card-title  text-primary-content">{props.title}</h2>
+        <p className=" text-primary-content">{props.subtitle}</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-accent btn-m">Ver Todos</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Categorie;
