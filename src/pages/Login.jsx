36a75ff456e985b387a3d8db5ad698890ffd75f0
Rony Romero Duque @ronygrafo <ronygrafo@icloud.com>
2023-11-02 18:05:10 -0500

    Signup y Login Forms

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,37 +1,70 @@
+import LogoCode from "../components/LogoCode";
+import { Link } from "react-router-dom";
+
 const Login = () => {
   return (
     <>
-    <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Login now!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-    </div>
-    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <form className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+      <div className="hero min-h-screen bg-base-200">
+        <div className="flex flex-col gap-8">
+          <LogoCode />
+
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="text-center lg:text-left mx-8">
+              <h1 className="text-5xl font-bold">Entrá Ahora!</h1>
+              <p className="py-6 w-72 font-bold">
+                Descubrí como podrás hacer <br />
+                <span className="text-accent">
+                  todo el trabajo pesado en un clic!
+                </span>
+              </p>
+            </div>
+            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+              <form className="card-body">
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="Ingresá tu email"
+                    className="input input-bordered"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Contraseña</span>
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="Ingresá tu contraseña"
+                    className="input input-bordered"
+                    required
+                  />
+                </div>
+                <p className="label-text my-4">
+                    Aún no tienes cuenta?
+                    <Link to="/Signup">
+                    <a href="#" className="text-sm label-text-alt link font-bold text-accent link-hover">
+                      {" "}
+                      Crea tu cuenta aquí{" "}
+                    </a>
+                    </Link>
+                  </p>
+                  <a href="#" className="text-sm label-text-alt link link-hover">
+                    Olvidaste tu contraseña?
+                  </a>
+                <div className="form-control mt-6">
+                  <button className="btn bg-primary text-base-100 hover:bg-yellow-500">
+                    INICIAR SESIÓN
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
+      </div>
     </>
-  )
-}
-export default Login
+  );
+};
+export default Login;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,37 +1,95 @@
+import LogoCode from "../components/LogoCode";
+import { Link } from "react-router-dom";
+
 const Signup = () => {
   return (
     <>
-    <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Login now!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-    </div>
-    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <form className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left mx-8 w-[380px]">
+            <h1 className="text-4xl font-bold">Registráte Ya!</h1>
+            <p className="py-6 w-full font-bold mb-8">
+            Y Disfrutá lo que es hacer <br />
+              <span className="text-accent">
+              todo el trabajo pesado en un clic!
+              </span>
+            </p>
+            <LogoCode />
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Nombre Completo</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Dorian Battiato"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="Ingresá tu email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Crea tu Contraseña</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="Crea una contraseña"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Confirma tu Contraseña</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="Confirmá tu contraseña"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label cursor-pointer flex-row-reverse justify-end gap-2">
+                  <span className="label-text">Acepto lo que tengo que aceptar</span>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </div>
+              <p className="label-text my-4">
+                Ya tienes cuenta?
+                <Link to="/Login">
+                <a
+                  href="#"
+                  className="text-sm label-text-alt link font-bold text-accent link-hover"
+                >
+                  {" "}
+                  Loguéate aquí{" "}
+                </a>
+                </Link>
+              </p>
+              <div className="form-control mt-6">
+                <button className="btn bg-primary text-base-100 hover:bg-yellow-500">
+                  CREAR CUENTA
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
+      </div>
     </>
-  )
-}
-export default Signup
+  );
+};
+export default Signup;
